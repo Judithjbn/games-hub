ed76024eff23963424a76ab687ed88f5ad74c1da
Judith <jbarrilnavarro@gmail.com>
2025-01-20 13:44:24 +0100

    gameCards actualización de componentes

--- src/components/GamesCards/GamesCards.js
+++ src/components/GamesCards/GamesCards.js
@@ -1,8 +1,27 @@
+import { cover } from '/src/import.js';
+// importar iconos de juegos para cards
+
+
 export function createCard(game) {
     const card = document.createElement('div');
     card.classList.add('game-card');
-    const img = document.createElement('img');
     
-//crear imagen como en header.
+    const img = document.createElement('img');
+    img.src = cover;
+    img.alt = game.title;
+    img.classList.add('game-card__image');
 
+    const title = document.createElement('h3');
+    title.textContent = game.title;
+    title.classList.add('game-card__title');
+
+    const description = document.createElement('p');
+    description.textContent = game.description;
+    description.classList.add('game-card__description');
+
+    card.appendChild(title);
+    card.appendChild(description);
+    card.appendChild(img);    
+
+    return card
 }
--- src/import.js
+++ src/import.js
@@ -1,9 +1,14 @@
 import Header from './components/Header/Header.js';
+import GamesGrid from './components/GamesCards/GamesGrid.js';
 
 // styles
 import './components/Header/Header.scss';
 
 // assets
-const logo = '/logo.png'; 
+const logo = '/logo.png';
+const cover = '/cover.png'; 
+// const iconTic
+// const iconTetris
+// const iconPac 
 
-export { Header, logo };
+export { Header, logo, cover };
